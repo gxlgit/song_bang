5d0c2fd4bd7b850185d3b5870a12dd90021b1b4b
Gwen Latasa <gxlatasa@gmail.com>
2018-02-16 09:07:15 -0500

    + seed data for genre/vibe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Genre.destroy_all
+Vibe.destroy_all
+Artist.destroy_all
+Song.destroy_all
+
+
+vibes = [  "Start This Party",
+          "Love Songs",
+          "Power Ballads",
+          "Slow Jams",
+          "Group Sing-a-long",
+          "Songs You Don't Actually Know the Words To",
+          "Breakup",
+          "Disney",
+          "80s Dance Party",
+          "Grunge Era",
+          "90s Hip Hop Fever",
+          "00s Popalicious",
+          "So Emo",
+          "Best Air Guitar Solos",
+          "Classics You Should Know"
+]
+
+vibes.map! do |vibe|
+  Vibe.create(name: vibe)
+end
+
+genres = ["Pop",
+          "Rock",
+          "Latin",
+          "R&B",
+          "Rap",
+          "Alternative",
+          "Indie",
+          "Broadway",
+          "Hip Hop",
+          "Blues",
+          "Soft Rock",
+          "Reggae",
+          "Ska",
+          "Country",
+          "Soul",
+          "Funk",
+          "Punk",
+          "Disco",
+          "New Wave",
+          "Folk"
+]
+
+genres.map! do |genre|
+  Genre.create(name: genre)
+end
